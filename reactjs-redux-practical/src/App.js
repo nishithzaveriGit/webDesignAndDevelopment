00d1b5ea20efb772f363562e8007b9b244a8010f
nishithzaveriGit <nishith_z@hotmail.com>
2023-11-22 21:26:04 +0530

    Remove unused code

--- reactjs-redux-practical/src/App.js
+++ reactjs-redux-practical/src/App.js
@@ -1,50 +1,28 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes } from 'react-router-dom';
 // import logo from './logo.svg';
 import './App.css';
 
-import Layout from "./components/Layout";
+import Layout from './components/Layout';
 
-import AddProduct from "./pages/AddProduct";
+import AddProduct from './pages/AddProduct';
 import ProductList from './pages/ProductList';
-import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
 
 function App() {
   return (
     <>
-    <Navbar />
-    <Routes>
-          <Route path="/" element={<Layout />} >
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Layout />}>
           {/* public routes */}
           <Route path="/" element={<Home />} />
           <Route path="/add-product" element={<AddProduct />} />
           <Route path="/list-product" element={<ProductList />} />
-          </Route>
-          
-        </Routes>
+        </Route>
+      </Routes>
     </>
-    
-  )
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //     <p className="">
-  //                   This is example for using button with
-  //                   bootstrap styling
-  //               </p>
-  //               <a
-  //                   className="btn btn-primary"
-  //                   data-bs-toggle="collapse"
-  //                   href="#collapseExample"
-  //                   role="button"
-  //                   aria-expanded="false"
-  //                   aria-controls="collapseExample"
-  //               >
-  //                   Bootstrap button
-  //               </a>
-  //     </header>
-  //   </div>
-  // );
+  );
 }
 
 export default App;
